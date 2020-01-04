a62bacdd42ab023be2d319e57178d85f4f1d1240
David Ikin <davidikin45@gmail.com>
2020-01-04 19:47:22 +0000

    changes

---
+++ src/Autofac.AspNetCore.Extensions/Data/IdentificationStrategies/DifferentSqliteDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using EntityFrameworkCore.Initialization;
+
+namespace Autofac.AspNetCore.Extensions.Data.IdentificationStrategies
+{
+    public class DifferentSqliteDbContext<TDbContext> : IDbContextTenantStrategy<TDbContext>
+        where TDbContext : DbContext
+    {
+        public string ConnectionStringName => null;
+
+        public void OnConfiguring(DbContextOptionsBuilder optionsBuilder, ITenant tenant, string tenantPropertyName)
+        {
+            var connectionString = $"Data Source={tenant.Id}.db;";
+            optionsBuilder.SetConnectionString<TDbContext>(connectionString);
+        }
+
+        public void OnModelCreating(ModelBuilder modelBuilder, DbContext context, ITenant tenant, string tenantPropertyName)
+        {
+          
+        }
+
+        public void OnSaveChanges(DbContext context, ITenant tenant, string tenantPropertyName)
+        {
+
+        }
+    }
+}
